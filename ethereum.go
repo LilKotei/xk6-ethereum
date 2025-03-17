74490eef9932a18b50a60947669b36c3e43c0e63
Ibrahim Kebe <ibrahim@e36knots.com>
2025-03-17 13:01:53 +0100

    chore: remove debbuging log

--- ethereum.go
+++ ethereum.go
@@ -129,7 +129,6 @@ func (c *Client) EstimateGas(tx Transaction) (uint64, error) {
 		Data:     tx.Input,
 		GasPrice: tx.GasPrice,
 	}
-	fmt.Printf("details: %+v\n", msg)
 	gas, err := c.client.Eth().EstimateGas(msg)
 	if err != nil {
 		return 0, fmt.Errorf("failed to estimate gas: %w", err)
