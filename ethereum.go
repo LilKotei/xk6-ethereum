d6b3dc14e91a6d2c9bd618e3f552d00fc198f0d3
Ibrahim Kebe <ibrahim@e36knots.com>
2025-03-20 15:19:56 +0100

    debug: moving printf

--- ethereum.go
+++ ethereum.go
@@ -164,11 +164,11 @@ func (c *Client) SendTransaction(tx Transaction) (string, error) {
 
 func (c *Client) SendRawTransaction(tx Transaction) (string, error) {
 	to := ethgo.HexToAddress(tx.To)
+	fmt.Printf("tx: %+v\n", tx)
 	gas, err := c.EstimateGas(tx)
 	if err != nil {
 		return "", err
 	}
-	fmt.Printf("tx: %+v\n", tx)
 	t := &ethgo.Transaction{
 		Type:     ethgo.TransactionLegacy,
 		From:     c.w.Address(),
